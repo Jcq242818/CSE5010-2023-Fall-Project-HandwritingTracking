fa6d80d86c91fdcc9308a625c9fa94b5b37fc6dd
Yuxiao Hua <1628280289@qq.com>
2023-11-22 00:03:13 +0800

    upload

---
+++ 代码/helperDetectionsMap.m
@@ -0,0 +1,8 @@
+function helperDetectionsMap(resp,rng_grid,dop_grid,rangeIndx,dopplerIndx,detections)
+    figure
+    detectionMap = zeros(size(resp));
+    detectionMap(rangeIndx(1):rangeIndx(2),dopplerIndx(1):dopplerIndx(2)) = ...
+      reshape(double(detections),rangeIndx(2)-rangeIndx(1)+1,dopplerIndx(2)-dopplerIndx(1)+1);
+    h = imagesc(dop_grid,rng_grid,detectionMap);
+    xlabel('Time (s)'); ylabel('Doppler (Hz)'); title('Range Doppler CFAR Detections');
+    h.Parent.YDir = 'normal';
