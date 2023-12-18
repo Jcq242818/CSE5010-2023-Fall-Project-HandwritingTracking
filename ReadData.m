458d104f98fec8103f7117289fa31da17871112c
Jcq242818 <11911303@mail.sustech.edu.cn>
2023-12-18 23:57:20 +0800

    update codes

--- ReadData.m
+++ ReadData.m
@@ -8,7 +8,7 @@ clc;
 
 chan_num_tar = 2;
 chan_num_ref = 1;
-total_duration = 9;
+total_duration = 4;
 
 f_c = 0.5e9;
 f_s = 1e6;
@@ -28,8 +28,8 @@ array_Doppler_frequency = -max_dop:step_dop:max_dop;
 chan_num_total = chan_num_tar+chan_num_ref;
 
 
-filename_data = 'E:\Desktop\Project\data\呼吸6次\6_1.bin';
-% filename_data='E:\0618_b\0618_b\b10_'+string(p)+'.bin';
+filename_data = "E:\Desktop\Project\data\big_8.bin";
+
 
 fid1=fopen(filename_data,'rb');  %rb - 二级制打开文件(1 Btye = 8 bits)
 fseek(fid1,0,'eof');  %eof  打开文件末尾
@@ -125,13 +125,13 @@ end
 
 %滑动窗口
 array_start_time = 0:T_slide:total_duration-CIT;
-% A_TD = zeros(length(array_start_time),length(array_Doppler_frequency));
-A_TD = zeros(length(array_start_time),length(-60:10:60));
+A_TD = zeros(length(array_start_time),length(array_Doppler_frequency));
+% A_TD = zeros(length(array_start_time),length(-60:10:60));
 ref_array=data_cor1(1,:); % ref_array里面装着参考对齐后的数据
 tar_array=data_cor1(2,:); % tar_array里面装着监视1对齐后的数据
 
-% A_TD2 = zeros(length(array_start_time),length(array_Doppler_frequency));
-A_TD2 = zeros(length(array_start_time),length(-60:10:60));
+A_TD2 = zeros(length(array_start_time),length(array_Doppler_frequency));
+% A_TD2 = zeros(length(array_start_time),length(-60:10:60));
 ref_array2=data_cor2(1,:); % ref_array2里面装着参考对齐后的数据,和ref_array里面的数据一模一样
 tar_array2=data_cor2(2,:); % tar_array2里面装着监视2对齐后的数据
 
@@ -158,11 +158,11 @@ end
 
 for i= 1:length(array_start_time)-2
     final=fftshift(fft(tar_integer(i,:).*conj(ref_integer(i,:)),CIT_region));
-    % A_TD(i,:) = final(CIT_region/2+1-max_dop/step_dop:CIT_region/2+1+max_dop/step_dop);
-    A_TD(i,:) = final(CIT_region/2+1-6:CIT_region/2+1+6);
-     final2=fftshift(fft(tar_integer2(i,:).*conj(ref_integer2(i,:)),CIT_region));
-    % A_TD2(i,:) = final2(CIT_region/2+1-max_dop/step_dop:CIT_region/2+1+max_dop/step_dop);
-    A_TD2(i,:) = final2(CIT_region/2+1-6:CIT_region/2+1+6);
+    A_TD(i,:) = final(CIT_region/2+1-max_dop/step_dop:CIT_region/2+1+max_dop/step_dop);
+    % A_TD(i,:) = final(CIT_region/2+1-6:CIT_region/2+1+6);
+    final2=fftshift(fft(tar_integer2(i,:).*conj(ref_integer2(i,:)),CIT_region));
+    A_TD2(i,:) = final2(CIT_region/2+1-max_dop/step_dop:CIT_region/2+1+max_dop/step_dop);
+    % A_TD2(i,:) = final2(CIT_region/2+1-6:CIT_region/2+1+6);
 end
 
 
@@ -178,16 +178,16 @@ fig1 = figure(1);
 set(fig1,'position',[50,50,900,600]);
 plot_A_DT = abs(A_TD');
 plot_A_DT = mag2db(plot_A_DT/max(max(plot_A_DT)));
-% h1 = imagesc(array_start_time,array_Doppler_frequency,plot_A_DT);
-h1 = imagesc(array_start_time,-60:10:60,plot_A_DT);
+h1 = imagesc(array_start_time,array_Doppler_frequency,plot_A_DT);
+% h1 = imagesc(array_start_time,-60:10:60,plot_A_DT);
 xlim([array_start_time(1),array_start_time(end)]);
-% ylim([array_Doppler_frequency(1),array_Doppler_frequency(end)]);
-ylim([-60,60]);
-% set(gcf,'unit','centimeters','position',[5 3 30 15]);
-% set(get(gca,'XLabel'),'FontSize',22);
-% set(get(gca,'YLabel'),'FontSize',22);
+ylim([array_Doppler_frequency(1),array_Doppler_frequency(end)]);
+% ylim([-60,60]);
+set(gcf,'unit','centimeters','position',[5 3 30 15]);
+set(get(gca,'XLabel'),'FontSize',22);
+set(get(gca,'YLabel'),'FontSize',22);
 colorbar;
-% title('1 and 2')
+title('ref and sur1')
 xlabel('Time (s)')
 ylabel('Doppler frequency (Hz)')
 colormap('jet');
@@ -195,21 +195,24 @@ caxis([thres_A_TRD,0]);
 % saveas(gcf, 'E:\0617\5s_'+string(p)+'-1fft.jpg', 'jpg')
 % saveas(gcf, 'C:\Users\Wu\Desktop\labsource\code\5a.jpg', 'jpg')
 
-% cfar2D = phased.CFARDetector2D('GuardBandSize',5,'TrainingBandSize',5,...
-%   'ProbabilityFalseAlarm',0.5);
-% resp=plot_A_DT;
-% rngGrid=array_Doppler_frequency.';
-% dopGrid=array_start_time.';
-% rangeIndx(1)=229;
-% rangeIndx(2)=287;
-% dopplerIndx(1)=11;
-% dopplerIndx(2)=150;
-% [columnInds,rowInds] = meshgrid(dopplerIndx(1):dopplerIndx(2),...
-%   rangeIndx(1):rangeIndx(2));
-% CUTIdx = [rowInds(:) columnInds(:)]';
-% detections = cfar2D(resp,CUTIdx);
-% fig2 = figure(2);
-% helperDetectionsMap(resp,rngGrid,dopGrid,rangeIndx,dopplerIndx,detections)
+
+cfar2D = phased.CFARDetector2D('GuardBandSize',5,'TrainingBandSize',5,...
+  'ProbabilityFalseAlarm',0.5);
+resp=plot_A_DT;
+rngGrid=array_Doppler_frequency.';
+dopGrid=array_start_time.';
+rangeIndx(1)=229;
+rangeIndx(2)=287;
+dopplerIndx(1)=11;
+dopplerIndx(2)=150;
+[columnInds,rowInds] = meshgrid(dopplerIndx(1):dopplerIndx(2),...
+  rangeIndx(1):rangeIndx(2));
+CUTIdx = [rowInds(:) columnInds(:)]';
+detections = cfar2D(resp,CUTIdx);
+fig2 = figure(2);
+helperDetectionsMap(resp,rngGrid,dopGrid,rangeIndx,dopplerIndx,detections)
+
+
 % %saveas(gcf, 'E:\0617\5s_'+string(p)+'-2cfar.jpg', 'jpg')
 % saveas(gcf, 'C:\Users\Wu\Desktop\labsource\code\5c.jpg', 'jpg')
 % 
@@ -220,43 +223,50 @@ caxis([thres_A_TRD,0]);
 % 
 % 
 % 
-% thres_A_TRD = -30;
-% fig3 = figure(3);
-% set(fig3,'position',[50,50,900,600]);
-% plot_A_DT2 = abs(A_TD2');
-% plot_A_DT2 = mag2db(plot_A_DT2/max(max(plot_A_DT2)));
-% h2 = imagesc(array_start_time,array_Doppler_frequency,plot_A_DT2);
-% xlim([array_start_time(1),array_start_time(end)]);
-% ylim([array_Doppler_frequency(1),array_Doppler_frequency(end)]);
-% set(gcf,'unit','centimeters','position',[5 3 30 15]);
-% set(get(gca,'XLabel'),'FontSize',22);
-% set(get(gca,'YLabel'),'FontSize',22);
-% colorbar;
-% xlabel('Time (s)')
-% ylabel('Doppler frequency (Hz)')
-% title('1 and 3')
-% colormap('jet');
-% caxis([thres_A_TRD,0]);
+thres_A_TRD = -30;
+fig3 = figure(3);
+set(fig3,'position',[50,50,900,600]);
+plot_A_DT2 = abs(A_TD2');
+plot_A_DT2 = mag2db(plot_A_DT2/max(max(plot_A_DT2)));
+h2 = imagesc(array_start_time,array_Doppler_frequency,plot_A_DT2);
+xlim([array_start_time(1),array_start_time(end)]);
+ylim([array_Doppler_frequency(1),array_Doppler_frequency(end)]);
+set(gcf,'unit','centimeters','position',[5 3 30 15]);
+set(get(gca,'XLabel'),'FontSize',22);
+set(get(gca,'YLabel'),'FontSize',22);
+colorbar;
+xlabel('Time (s)')
+ylabel('Doppler frequency (Hz)')
+title('ref and sur2')
+colormap('jet');
+caxis([thres_A_TRD,0]);
+hold on;
+
+
 %saveas(gcf, 'E:\0617\5s_'+string(p)+'-3fft.jpg', 'jpg')
 % saveas(gcf, 'C:\Users\Wu\Desktop\labsource\code\5b.jpg', 'jpg')
 % 
 % 
 
-% cfar2D = phased.CFARDetector2D('GuardBandSize',5,'TrainingBandSize',5,...
-%   'ProbabilityFalseAlarm',0.5);
-% resp=plot_A_DT2;
-% rngGrid=array_Doppler_frequency.';
-% dopGrid=array_start_time.';
-% rangeIndx(1)=229;
-% rangeIndx(2)=287;
-% dopplerIndx(1)=11;
-% dopplerIndx(2)=150;
-% [columnInds,rowInds] = meshgrid(dopplerIndx(1):dopplerIndx(2),...
-%   rangeIndx(1):rangeIndx(2));
-% CUTIdx = [rowInds(:) columnInds(:)]';
-% detections = cfar2D(resp,CUTIdx);
-% fig4 = figure(4);
-% helperDetectionsMap(resp,rngGrid,dopGrid,rangeIndx,dopplerIndx,detections)
+cfar2D = phased.CFARDetector2D('GuardBandSize',5,'TrainingBandSize',5,...
+  'ProbabilityFalseAlarm',0.5);
+resp=plot_A_DT2;
+rngGrid=array_Doppler_frequency.';
+dopGrid=array_start_time.';
+rangeIndx(1)=229;
+rangeIndx(2)=287;
+dopplerIndx(1)=11;
+dopplerIndx(2)=150;
+[columnInds,rowInds] = meshgrid(dopplerIndx(1):dopplerIndx(2),...
+  rangeIndx(1):rangeIndx(2));
+CUTIdx = [rowInds(:) columnInds(:)]';
+detections = cfar2D(resp,CUTIdx);
+fig4 = figure(4);
+helperDetectionsMap(resp,rngGrid,dopGrid,rangeIndx,dopplerIndx,detections)
+
+
+
+
 % saveas(gcf, 'E:\0617\5s_'+string(p)+'-4cfar.jpg', 'jpg')
 % saveas(gcf, 'C:\Users\Wu\Desktop\labsource\code\5f.jpg', 'jpg')
 % close all;
