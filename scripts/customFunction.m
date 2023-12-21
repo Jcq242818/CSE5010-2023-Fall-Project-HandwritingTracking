5f169b543c969992eaf50a5894d2d0951b70d146
Yuxiao Hua <1628280289@qq.com>
2023-12-22 01:01:15 +0800

    Update

--- scripts/customFunction.m
+++
@@ -1,9 +0,0 @@
-function y = customFunction(x)
-    if x >= 1 && x <= 100
-        y = 10 * (x - 1) - 1000;
-    elseif x >= 101 && x <= 200
-        y = 10 * (x - 100);
-    else
-        error('Input x is out of the specified range.');
-    end
-end
--- scripts/main.m
+++ scripts/main.m
@@ -1,4 +1,4 @@
-%% 参数定义
+%% 
 clear ;
 close all;
 clc;
@@ -11,56 +11,59 @@ total_duration = 4;
 
 f_c = 0.5e9;
 f_s = 1e6;
+%
+c = 3e8;
+%
 
-CIT = 0.1; %跟模糊函数的数据量有关
+CIT = 0.1; %
 CIT_region = CIT*f_s;
 
 
 N_slide = 10;
-T_slide = CIT / N_slide;%将 0.1s 的数据切割成N_slide 份，滑动的步长
+T_slide = CIT / N_slide;% 0.1s N_slide 
 
 max_dop = 1000;
 step_dop = 1/CIT;
 array_Doppler_frequency = -max_dop:step_dop:max_dop;
 
-% filename_data = "D:\Github\Passive-Handwriting-Tracking\data\0413\0_small.bin";
+filename_data = "D:\Github\Passive-Handwriting-Tracking\data\0413\0_small.bin";
 % filename_data = "D:\Github\Passive-Handwriting-Tracking\data\0413\3.bin";
 % filename_data = "D:\Github\Passive-Handwriting-Tracking\data\0413\7.bin";
-filename_data = "D:\Github\Passive-Handwriting-Tracking\data\0413\8.bin";
+% filename_data = "D:\Github\Passive-Handwriting-Tracking\data\0413\8.bin";
 
-fid1=fopen(filename_data,'rb');  %rb - 二级制打开文件(1 Btye = 8 bits)
-fseek(fid1,0,'eof');  %eof  打开文件末尾
-fsize = ftell(fid1);  %获取文件长度
+fid1=fopen(filename_data,'rb');  %rb - (1 Btye = 8 bits)
+fseek(fid1,0,'eof');  %eof  
+fsize = ftell(fid1);  %
 
-total_samplelen = fsize/4;  % Byte/8 * 2， IQ分离
+total_samplelen = fsize/4;  % Byte/8 * 2 IQ
 
-sample_1channel = 1996; %unit 大小  unit单位内IQ交叠 每一个unit只属于一个channel 单位应该是byte
+sample_1channel = 1996; %unit   unitIQ unitchannel byte
 
-samplelen = sample_1channel * (chan_num_total); %一个block 的长度= channel数量*单位长度
+samplelen = sample_1channel * (chan_num_total); %block = channel*
 %sampleBytes = samplelen*4;
-loopNum = floor(total_samplelen/samplelen); %循环次数 = 向下取整(总共长度/一个block长度)
+loopNum = floor(total_samplelen/samplelen); % = (/block)
 
-samples_per_chan = sample_1channel * loopNum ;%一个channel采样点数:单位长度*循环次数(反复采样了多少次)，用于后面创建一个矩阵来存放数据
+samples_per_chan = sample_1channel * loopNum ;%channel:*()
 %DurationTruth = samples_per_chan/f_s;
 fclose(fid1);
 
 
 %% Read Calibration File
-fid_cali=fopen(filename_data,'rb');%二进制byte打开文件
-fseek(fid_cali,0,'bof');%bof 打开文件头
-chan_seq_cali = zeros(chan_num_total,samples_per_chan);% 3xX 矩阵
+fid_cali=fopen(filename_data,'rb');%byte
+fseek(fid_cali,0,'bof');%bof 
+chan_seq_cali = zeros(chan_num_total,samples_per_chan);% 3xX 
 for loopIdx = 1:loopNum
-    [data,~]=fread(fid_cali,[2,sample_1channel*chan_num_total],'int16','l');% 竖着从左至右填满数组2xX，IQ分解 int16 16字节数
+    [data,~]=fread(fid_cali,[2,sample_1channel*chan_num_total],'int16','l');% 2xXIQ int16 16
     for idx_chan = 0:chan_num_total-1
         frame_start = sample_1channel*(0 + idx_chan)+1;
         frame_end = sample_1channel*(1 + idx_chan);
-        chan_seq_cali(idx_chan+1,(loopIdx-1)*sample_1channel+1:loopIdx*sample_1channel) = ...%分三组写入数据
-            data(1,frame_start:frame_end) + 1i*data(2,frame_start:frame_end);%雀食是IQ分解，把之前的IQ分解组合
+        chan_seq_cali(idx_chan+1,(loopIdx-1)*sample_1channel+1:loopIdx*sample_1channel) = ...%
+            data(1,frame_start:frame_end) + 1i*data(2,frame_start:frame_end);%IQIQ
     end
 end
 fclose(fid_cali);
 
-%% 数据处理
+%% 
 data_count = 2184*1;
 S_ref = chan_seq_cali(3,1:data_count);
 S_tar = chan_seq_cali(1,1:data_count);
@@ -69,7 +72,7 @@ h_temp = xcorr(S_ref,S_tar);
 col_h = col_h +1;
 
 
-%提取信道
+%
 S_tar=chan_seq_cali(1,:);
 S_ref1=chan_seq_cali(2,:);
 S_ref2=chan_seq_cali(3,:);
@@ -90,27 +93,27 @@ else
 end
 
 
-%滑动窗口
+%
 array_start_time = 0:T_slide:total_duration-CIT;
 A_TD = zeros(length(array_start_time),length(array_Doppler_frequency));
-ref_array=data_cor1(1,:); % ref_array里面装着参考对齐后的数据
-tar_array=data_cor1(2,:); % tar_array里面装着监视1对齐后的数据
+ref_array=data_cor1(1,:); % ref_array
+tar_array=data_cor1(2,:); % tar_array1
 
 A_TD2 = zeros(length(array_start_time),length(array_Doppler_frequency));
-ref_array2=data_cor2(1,:); % ref_array2里面装着参考对齐后的数据,和ref_array里面的数据一模一样
-tar_array2=data_cor2(2,:); % tar_array2里面装着监视2对齐后的数据
+ref_array2=data_cor2(1,:); % ref_array2,ref_array
+tar_array2=data_cor2(2,:); % tar_array22
 
-%数据处理
+%
 ref_integer=zeros(length(array_start_time),CIT_region);
-tar_integer=zeros(length(array_start_time),CIT_region);%准备fft数组表
+tar_integer=zeros(length(array_start_time),CIT_region);%fft
 
 ref_integer2=zeros(length(array_start_time),CIT_region);
-tar_integer2=zeros(length(array_start_time),CIT_region);%准备fft数组表
+tar_integer2=zeros(length(array_start_time),CIT_region);%fft
 
 for i= 1:length(array_start_time)-2
     ref_integer(i,:)=ref_array((round(array_start_time(i)*f_s+1)):(round(array_start_time(i)*f_s)+round(CIT*f_s)));
     tar_integer(i,:)=tar_array((round(array_start_time(i)*f_s+1)):(round(array_start_time(i)*f_s)+round(CIT*f_s)));
-end                                                    %分类并填写fft数组表
+end                                                    %fft
 
 for i= 1:length(array_start_time)-2
     ref_integer2(i,:)=ref_array2((round(array_start_time(i)*f_s+1)):(round(array_start_time(i)*f_s)+round(CIT*f_s)));
@@ -130,10 +133,9 @@ end
 
 
 
-%% 绘制CAF图
+%% CAF
 
-
-%%画ref和sur1的CAF
+%%refsur1CAF
 thres_A_TRD = -30;
 fig1 = figure(1);
 set(fig1,'position',[50,50,900,600]);
@@ -142,7 +144,7 @@ plot_A_DT = mag2db(plot_A_DT/max(max(plot_A_DT)));
 h1 = imagesc(array_start_time,array_Doppler_frequency,plot_A_DT);
 xlim([array_start_time(1),array_start_time(end)]);
 ylim([-100, 100]);
-% 反转y轴方向
+% y
 set(gca, 'YDir', 'normal');
 set(gcf,'unit','centimeters','position',[5 3 30 15]);
 set(get(gca,'XLabel'),'FontSize',22);
@@ -155,7 +157,7 @@ colormap('jet');
 clim([thres_A_TRD,0]);
 
 
-%%画ref和sur2的CAF 
+%%refsur2CAF 
 thres_A_TRD = -30;
 fig2 = figure(2);
 set(fig2,'position',[50,50,900,600]);
@@ -164,7 +166,7 @@ plot_A_DT2 = mag2db(plot_A_DT2/max(max(plot_A_DT2)));
 h2 = imagesc(array_start_time,array_Doppler_frequency,plot_A_DT2);
 xlim([array_start_time(1),array_start_time(end)]);
 ylim([-100, 100]);
-% 反转y轴方向
+% y
 set(gca, 'YDir', 'normal');
 set(gcf,'unit','centimeters','position',[5 3 30 15]);
 set(get(gca,'XLabel'),'FontSize',22);
@@ -176,22 +178,22 @@ title('ref and sur2')
 colormap('jet');
 clim([thres_A_TRD,0]);
 
-%% 提取CAF结果每一列的最大值并进行迭代
+%% CAF
 
-% 最后两行数据是-Inf，删除
+% -Inf
 A_TD = A_TD(1:end-2, :);
 A_TD2 = A_TD2(1:end-2,:);
-% 删除零频附近的值
+% 
 A_TD(:, 101) = [];
 A_TD2(:, 101) = [];
-%%重要，这个删去无关信息的起始时间delete_index需要自适应地动态调整
+%%delete_index
 delete_index = 51;
 A_TD = A_TD(delete_index:end,:);
 A_TD2 = A_TD2(delete_index:end,:);
-%%在画图对比一下
+%%
 
 
-%画ref和sur1的CAF
+%refsur1CAF
 thres_A_TRD = -30;
 fig3 = figure(3);
 set(fig3,'position',[50,50,900,600]);
@@ -200,7 +202,7 @@ plot_A_DT = mag2db(plot_A_DT/max(max(plot_A_DT)));
 h1 = imagesc(array_start_time,array_Doppler_frequency,plot_A_DT);
 xlim([array_start_time(1),array_start_time(end)]);
 ylim([-100, 100]);
-% 反转y轴方向
+% y
 set(gca, 'YDir', 'normal');
 set(gcf,'unit','centimeters','position',[5 3 30 15]);
 set(get(gca,'XLabel'),'FontSize',22);
@@ -213,7 +215,7 @@ colormap('jet');
 clim([thres_A_TRD,0]);
 
 
-%画ref和sur2的CAF 
+%refsur2CAF 
 thres_A_TRD = -30;
 fig4 = figure(4);
 set(fig4,'position',[50,50,900,600]);
@@ -222,7 +224,7 @@ plot_A_DT2 = mag2db(plot_A_DT2/max(max(plot_A_DT2)));
 h2 = imagesc(array_start_time,array_Doppler_frequency,plot_A_DT2);
 xlim([array_start_time(1),array_start_time(end)]);
 ylim([-100, 100]);
-% 反转y轴方向
+% y
 set(gca, 'YDir', 'normal');
 set(gcf,'unit','centimeters','position',[5 3 30 15]);
 set(get(gca,'XLabel'),'FontSize',22);
@@ -233,134 +235,170 @@ ylabel('Doppler frequency (Hz)')
 title('ref and sur2')
 colormap('jet');
 clim([thres_A_TRD,0]);
+close('all')
 
-
-%% 提取两个CAF矩阵每一行的最大值所在的列
-% 找到每一行中的最大值及其对应的列索引
+%% CAF
+% 
 [maxValues, columnIndices] = max(abs(A_TD), [], 2);
 [maxValues2, columnIndices2] = max(abs(A_TD2), [], 2);
-% columnIndices 就是每一行中最大值所在的列数,把列数置换成对应的多普勒频率
+% columnIndices ,
 maxDF1 = (columnIndices - 101) * step_dop;
 maxDF2 = (columnIndices2 - 101) * step_dop;
 
+%% 
+% 对maxDF做积分
+cumsumMaxDF = zeros(2,339);
+cumsumMaxDF(1,:) = cumsum(maxDF1');
+cumsumMaxDF(2,:) = cumsum(maxDF2');
+
+% save('D:\Github\Passive-Handwriting-Tracking\data\train\data.mat', 'cumsumMaxDF');
+
+% 对cumsum做卡尔曼平滑
+cumsumMaxDF_KS = zeros(2,339);
+cumsumMaxDF_KS(1,:) = KalmanSmoother(cumsumMaxDF(1,:));
+cumsumMaxDF_KS(2,:) = KalmanSmoother(cumsumMaxDF(2,:));
+
 figure(7);
-plot(maxDF1, '-','LineWidth', 2);
-ylim([-100, 100]);
-yticks(-100:10:100);
+plot(cumsumMaxDF(1,:), '-', 'LineWidth', 2, 'DisplayName', 'Channel 1');
 hold on;
-plot(maxDF2, '-','LineWidth', 2);
-ylim([-100, 100]);
-yticks(-100:10:100);
+plot(cumsumMaxDF(2,:), '-', 'LineWidth', 2, 'DisplayName', 'Channel 2');
+hold on;
+plot(cumsumMaxDF_KS(1,:), '-', 'LineWidth', 2, 'DisplayName', 'Channel 1 KS');
+hold on;
+plot(cumsumMaxDF_KS(2,:), '-', 'LineWidth', 2, 'DisplayName', 'Channel 2 KS');
 hold off;
+
+legend('show');
 grid on;
 
-%% 
-cumsumMazDF = zeros(2,339);
-cumsumMazDF(1,:) = cumsum(maxDF1');
-cumsumMazDF(2,:) = cumsum(maxDF2');
 
-save('D:\Github\Passive-Handwriting-Tracking\data\train\data.mat', 'cumsumMazDF');
+% 对cumsum做微分
+maxDF1_KS = diff(cumsumMaxDF_KS(1,:));
+maxDF2_KS = diff(cumsumMaxDF_KS(2,:));
 
 figure(8);
-plot(cumsum(maxDF1), '-', 'LineWidth', 2);
-
+plot(maxDF1, '-','LineWidth', 2, 'DisplayName', 'Channel 1 original');
 hold on;
-plot(cumsum(maxDF2), '-','LineWidth', 2);
+plot(maxDF2, '-','LineWidth', 2, 'DisplayName', 'Channel 2 original');
+hold on;
+plot(maxDF1_KS, '-','LineWidth', 2, 'DisplayName', 'Channel 1 KS undirect');
+hold on;
+plot(maxDF2_KS, '-','LineWidth', 2, 'DisplayName', 'Channel 2 KS undirect');
+hold on;
 
+% 对比直接做卡尔曼平滑
+maxDF_KS = zeros(2,339);
+maxDF_KS(1,:) = KalmanSmoother(maxDF1);
+maxDF_KS(2,:) = KalmanSmoother(maxDF2);
+
+plot(maxDF_KS(1,:), '-','LineWidth', 2, 'DisplayName', 'Channel 1 KS direct');
+hold on;
+plot(maxDF_KS(2,:), '-','LineWidth', 2, 'DisplayName', 'Channel 2 KS direct');
 hold off;
+
+ylim([-150, 150]);
+yticks(-150:10:150);
+legend('show');
 grid on;
 
-%% 初始位置确定
-%确定物体和收发机的初始位置
-% 定义收发机位置
-%发射机位置 
+%% 
+% 
 xT = 0;
 yT = 0;
-%sur1的位置
+%sur1
 xR1 = 3;
 yR1 = 0;
-%sur2的位置--假设sur2和tx呈45度
+%sur2--sur2tx45
 xR2 = sqrt(2);
 yR2 = -sqrt(2);
-%物体的初始位置
+%
 xTar = 2;
 yTar = -sqrt(2);
 
-sensing_time = array_start_time(delete_index:end);
-sensing_time = sensing_time - (delete_index -1 ) * 0.01;
-%%角度初始化
-fai_sur1 = zeros(1,length(sensing_time)-1);
-fai_sur2 = zeros(1,length(sensing_time)-1);
-fai_tx = zeros(1,length(sensing_time)-1);
+
+fai_sur1 = zeros(1,339);
+fai_sur2 = zeros(1,339);
+fai_tx = zeros(1,339);
 
 fai_sur1(1) = atan((yTar)/ (xTar - xR1));
 fai_sur2(1) = atan((yTar - yR2)/ (xTar - xR2));
 fai_tx(1) = atan(yTar/xTar);
 
-%存放每一个时刻目标的位置
-xtar = zeros(1,length(sensing_time)-1);
-ytar = zeros(1,length(sensing_time)-1);
+fai_sur1(1) = atan((yTar)/ (xTar - xR1));
+fai_sur2(1) = atan((yTar - yR2)/ (xTar - xR2));
+fai_tx(1) = atan(yTar/xTar);
+
+%
+xtar = zeros(1,339);
+ytar = zeros(1,339);
 xtar(1) = xTar;
 ytar(1) = yTar;
-%存放方程每一次迭代解的xy方向速度值
-v_xy = zeros(length(sensing_time)-2,2);
-%存放两条链路多普勒频率的临时变量
+
+%xy
+v_xy = zeros(339,2);
+vMax = 0.5;
+
+%
 fd = zeros(1,2);
-%解方程需要的F矩阵,参见ICCC论文
+%F,ICCC
 F = zeros(2,2);
-%载波频率
-fc = 60.48e9;
-%光速
-c = 3e8;
-%开始解方程与迭代
-for i = 1:1:length(sensing_time)-2
-    fd = [maxDF1(i);maxDF2(i)];
-    F = -2*fc/c*[cos((fai_sur1(i) - fai_tx(i))/2)*cos((fai_sur1(i) + fai_tx(i))/2) , ...
-        cos((fai_sur1(i) - fai_tx(i))/2)*sin((fai_sur1(i) + fai_tx(i))/2); ...
-        cos((fai_sur2(i) - fai_tx(i))/2)*cos((fai_sur2(i) + fai_tx(i))/2), ...
-        cos((fai_sur2(i) - fai_tx(i))/2)*sin((fai_sur2(i) + fai_tx(i))/2)];
+
+
+v_xy(1,:) = [0.1, 0.1];
+
+for i = 1:1:338
+    fd = [maxDF1_KS(i); maxDF2_KS(i)];
+    F = -2*f_c/c*[cos((fai_sur1(i) - fai_tx(i))/2) * cos((fai_sur1(i) + fai_tx(i))/2) , ...
+                 cos((fai_sur1(i) - fai_tx(i))/2) * sin((fai_sur1(i) + fai_tx(i))/2); ...
+                 cos((fai_sur2(i) - fai_tx(i))/2) * cos((fai_sur2(i) + fai_tx(i))/2), ...
+                 cos((fai_sur2(i) - fai_tx(i))/2) * sin((fai_sur2(i) + fai_tx(i))/2)];
     v_xy(i,:) = F \ fd;
 
-    % vMax = 0.3;
-    % for index = [1, 2]
-    % %判断解是否正确，如果解出来离谱的速度值，就要用之前的值进行加权平均
-    % if abs(v_xy(i,index)) > vMax
-    %     v_xy(i,1) = (v_xy(i-1,1) + v_xy(i-2,1) + v_xy(i-3,1))/3;
+
+    %
+    % if (abs(v_xy(i,1)) > vMax) || (abs(v_xy(i,2)) > vMax)
+    %     v_xy(i,:) = (v_xy(i-1,:) + v_xy(i-2,:) + v_xy(i-3,:))/3;
+    %     v_xy(i,:) = sign(x_hat(1)) * vMax;
     % end
-    % end
+
+    if abs(v_xy(i,1)) > vMax
+        v_xy(i,1) = sign(v_xy(i,1)) * vMax;
+    end
+    if abs(v_xy(i,2)) > vMax
+        v_xy(i,2) = sign(v_xy(i,2)) * vMax;
+    end
 
     xtar(i+1) = xtar(i) + v_xy(i,1) * T_slide;
     ytar(i+1) = ytar(i) + v_xy(i,2) * T_slide;
 
     %更新下一时刻的AOA角度以便下一次进行迭代
-    fai_sur1(i+1) = atan((ytar(i+1))/ (xtar(i+1) - xR1));
-    fai_sur2(i+1) = atan((ytar(i+1) - yR2)/ (xtar(i+1) - xR2));
-    fai_tx(i+1) = atan(ytar(i+1)/xtar(i+1));
+    fai_sur1(i+1) = atan((ytar(i+1) - yR1) / (xtar(i+1) - xR1));
+    fai_sur2(i+1) = atan((ytar(i+1) - yR2) / (xtar(i+1) - xR2));
+    fai_tx(i+1) = atan(ytar(i+1) / xtar(i+1));
 end
 
-figure(5);
-plot(v_xy(:,1));
+
+figure(9);
+plot(v_xy(:,1),'LineWidth', 2, 'DisplayName', 'vx');
 hold on;
-plot(v_xy(:,2));
+plot(v_xy(:,2),'LineWidth', 2, 'DisplayName', 'vy');
 hold off;
 legend('show');
 
-%% 绘制运动物体的轨迹
-% 绘制轨迹
-figure(6);
+%% 
+% 
+figure(10);
 plot(xtar, ytar, '-','Color', [1, 0.5, 0],'LineWidth', 3,'DisplayName', 'Estimated Trajectory');
-% 设置图形标题和坐标轴标签
+% 
 hold on;
 plot(xtar(1), ytar(1), 'o', 'MarkerSize', 6, 'MarkerEdgeColor', 'r',...
     'MarkerFaceColor', 'none', 'LineWidth', 2.5,'DisplayName', 'Initial Point');
-plot(xtar(end), ytar(end), 'x', 'MarkerSize', 8, 'MarkerEdgeColor', 'b',...
+plot(xtar(end), ytar(end), 'x', 'MarkerSize', 6, 'MarkerEdgeColor', 'b',...
     'MarkerFaceColor', 'none', 'LineWidth', 2.5,'DisplayName', 'End Point');
-title('物体运动轨迹');
-xlabel('X坐标');
-ylabel('Y坐标');
 hold off;
+
 legend('show','Location', 'northeast');
 axis equal;
-% 显示网格
+% 
 grid on;
 
