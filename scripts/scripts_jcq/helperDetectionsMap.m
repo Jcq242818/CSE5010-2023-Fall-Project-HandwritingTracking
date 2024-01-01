15d08e21956496c0a07db0f7333829ae0601dd3f
Yuxiao Hua <1628280289@qq.com>
2024-01-01 21:45:47 +0800

    Chdir and download some files

--- scripts/scripts_jcq/helperDetectionsMap.m
+++ scripts/scripts_jcq/helperDetectionsMap.m
@@ -4,10 +4,13 @@ function helperDetectionsMap(resp,rng_grid,dop_grid,rangeIndx,dopplerIndx,detect
 
 %   Copyright 2016 The MathWorks, Inc.
 
-figure
+figure;
 detectionMap = zeros(size(resp));
 detectionMap(rangeIndx(1):rangeIndx(2),dopplerIndx(1):dopplerIndx(2)) = ...
   reshape(double(detections),rangeIndx(2)-rangeIndx(1)+1,dopplerIndx(2)-dopplerIndx(1)+1);
 h = imagesc(dop_grid,rng_grid,detectionMap);
+
+% Modifided by Eason Hua
+ylim([-300,300]);
 xlabel('Time (s)'); ylabel('Doppler (Hz)'); title('Range Doppler CFAR Detections');
 h.Parent.YDir = 'normal';
